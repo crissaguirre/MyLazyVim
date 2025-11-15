2c6930b77e2b7d67fd8182e82473568ba2a6373b
Cristian Aguirre <caguirre@btg.com.pe>
2025-11-15 11:15:44 -0500

    actualizando

--- lua/config/codecompanion.lua
+++ lua/config/codecompanion.lua
@@ -1,46 +1,46 @@
 require("codecompanion").setup({
-	adapters = {
-		openrouter = function()
-			return require("codecompanion.adapters").extend("openai", {
-				name = "openrouter",
-				url = "https://openrouter.ai/api/v1/chat/completions",
-				env = {
-					api_key = os.getenv("OPENROUTER_API_KEY"),
-				},
-				headers = {
-					["HTTP-Referer"] = "https://neovim.org",
-				},
-				schema = {
-					model = {
-						default = "deepseek/deepseek-chat-v3.1:free",
-						choices = {
-							"deepseek/deepseek-chat-v3.1:free",
-							"nvidia/nemotron-nano-9b-v2:free",
-							"openai/gpt-oss-20b:free",
-						},
-					},
-					max_tokens = {
-						default = 2000,
-					},
-				},
-			})
-		end,
-	},
-	strategies = {
-		chat = {
-			adapter = "openrouter",
-			-- model = "deepseek/deepseek-chat-v3.1:free",
-		},
-		inline = {
-			adapter = "openrouter",
-			-- model = "deepseek/deepseek-chat-v3.1:free",
-		},
-		cmd = {
-			adapter = "openrouter",
-			-- model = "deepseek/deepseek-chat-v3.1:free",
-		},
-	},
-	opts = {
-		log_level = "DEBUG",
-	},
+  adapters = {
+    openrouter = function()
+      return require("codecompanion.adapters").extend("openai", {
+        name = "openrouter",
+        url = "https://openrouter.ai/api/v1/chat/completions",
+        env = {
+          api_key = os.getenv("OPENROUTER_API_KEY"),
+        },
+        headers = {
+          ["HTTP-Referer"] = "https://neovim.org",
+        },
+        schema = {
+          model = {
+            default = "nvidia/nemotron-nano-9b-v2:free",
+            choices = {
+              "deepseek/deepseek-chat-v3.1:free",
+              "nvidia/nemotron-nano-9b-v2:free",
+              "openai/gpt-oss-20b:free",
+            },
+          },
+          max_tokens = {
+            default = 2000,
+          },
+        },
+      })
+    end,
+  },
+  strategies = {
+    chat = {
+      adapter = "openrouter",
+      -- model = "deepseek/deepseek-chat-v3.1:free",
+    },
+    inline = {
+      adapter = "openrouter",
+      -- model = "deepseek/deepseek-chat-v3.1:free",
+    },
+    cmd = {
+      adapter = "openrouter",
+      -- model = "deepseek/deepseek-chat-v3.1:free",
+    },
+  },
+  opts = {
+    log_level = "DEBUG",
+  },
 })
