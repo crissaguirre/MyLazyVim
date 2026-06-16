e6481feafb3d1e2671ddfc70104c893c21c7e1a6
Cristian Aguirre <cristian.aguirre@fibernaas.onmicrosoft.com>
2026-06-16 17:03:31 -0500

    updated

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -9,16 +9,4 @@ return {
       require("config.codecompanion")
     end,
   },
-  -- {
-  --   "HakonHarnes/img-clip.nvim",
-  --   opts = {
-  --     filetypes = {
-  --       codecompanion = {
-  --         prompt_for_file_name = false,
-  --         template = "[Image]($FILE_PATH)",
-  --         use_absolute_path = true,
-  --       },
-  --     },
-  --   },
-  -- },
 }
